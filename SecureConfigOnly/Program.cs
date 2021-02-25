30922c1a73326830851c32cc872340605207270c
stauent <stauent@yahoo.ca>
2021-02-25 16:14:03 -0500

    Comment to warn that app won't run while connected through VPN

--- SecureConfigOnly/Program.cs
+++ SecureConfigOnly/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using SecureHostBuilderHelper;
 
 namespace SecureConfigOnly
@@ -7,6 +8,8 @@ namespace SecureConfigOnly
     {
         static async Task Main(string[] args)
         {
+            Console.WriteLine("Ensure that you are not running in a VPN or that appropriate ports are open to access Azure Resources.");
+
             ConfigurationResults<MyApplication> _configuredApplication = HostBuilderHelper.CreateApp<MyApplication>(args);
             await _configuredApplication.myService.Run();
         }
